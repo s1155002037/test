a81b8424a4f6e9093e23567bf7406c2a9d1eda08
flymike <liquanquan.michael@gmail.com>
2015-02-02 18:52:25 +0800

    test init

---
+++ app.js
@@ -0,0 +1,6 @@
+var http = require("http");
+http.createServer(function(request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Mike");
+  response.end();
+}).listen(process.env.PORT||3000);
